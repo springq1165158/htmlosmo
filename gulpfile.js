2a9c7d2b6bc2181968f5ccc55fe20900855fb5e8
unknown <thieuquangvinhz@gmail.com>
2019-05-10 17:38:03 +0700

    start nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const gulp = require('gulp');
 const gulpsass = require('gulp-sass');
 const gulpcopy = require('gulp-copy');
-const browserSync = require('browser-sync');
+const gulpImageMin = require('gulp-imagemin');
+const nunjucks = require('gulp-nunjucks');
+const browserSync = require('browser-sync').create();
 //init libary
 gulpsass.compiler = require ('node-sass');
 //---config structure 
 const APP_SRC_PATH = './src';
 const APP_TMP_PATH = './tmp';
+const APP_DIST_PATH = './dist';
 const APP_STRUCTURE = {
     tmp :{
         css     : APP_TMP_PATH + '/css',
@@ -14,11 +17,17 @@ const APP_STRUCTURE = {
         fonts   : APP_TMP_PATH + '/fonts',
         js      : APP_TMP_PATH + '/js',
     },
-    src: {
+    src :{
         scss    :  APP_SRC_PATH + '/scss',
         images    :  APP_SRC_PATH + '/images',
         fonts   :   APP_SRC_PATH + '/fonts',
         js      : APP_SRC_PATH + '/js',
+    },
+    dist:{
+        scss    :  APP_DIST_PATH + '/scss',
+        images    :  APP_DIST_PATH + '/images',
+        fonts   :   APP_DIST_PATH + '/fonts',
+        js      : APP_DIST_PATH + '/js',
     }
 }
 //--Compiler scss
@@ -51,15 +60,46 @@ gulp.task('copy:js',function(){
         prefix:2,
     }))
 })
+//--image min
+gulp.task('image:min',function(){
+    return gulp.src(APP_STRUCTURE.src.images + '/**/*.{jpg,png,gif,svg}')
+    .pipe(gulpImageMin())
+    .pipe(gulp.dest(APP_STRUCTURE.dist.images));
+});
+//--auto reload
+gulp.task('browserSync:start',function(){
+    return browserSync.init({
+        reloadDelay:300,
+        open:false,
+        notify:{
+            styles:[
+                'display:none'
+            ]
+        },
+        server:{
+            baseDir:APP_TMP_PATH,
+            index:index.html,
+        }
+    })
+});
+gulp.task('browserSync:reload',function(){
+    brownserSync.reload();
+    return done();
+})
 //--watch change
 gulp.task('watchchange',function()
 {
-    gulp.watch(APP_STRUCTURE.src.scss +'/**/*.scss',gulp.series('compileScss'))
+    gulp.watch(APP_STRUCTURE.src.scss +'/**/*.scss',gulp.series('compileScss','browserSync:reload'))
     gulp.watch(APP_STRUCTURE.src.images +'/**/*.{jpg,png,gif,svg}',{
         events: ['add','change'],
-    },gulp.series('copy:images'))
+    },gulp.series('copy:images','browserSync:reload'))
     gulp.watch(APP_STRUCTURE.src.fonts + '/**/*.{otf,eot,svg,ttf,woff,woff2}',{
         events: ['add','change'],
-    },gulp.series('copy:fonts'))
-    gulp.watch(APP_STRUCTURE.src.js +'/**/*.js',gulp.series('copy:js'))
+    },gulp.series('copy:fonts','browserSync:reload'))
+    gulp.watch(APP_STRUCTURE.src.js +'/**/*.js',gulp.series('copy:js','browserSync:reload'))
 })
+
+// 1. Compile .njk to html
+// 2. Viết cho tạo file .scss chỉ build đúng file mình sửa
+// 3. Viết hoàn tất môi trường dev
+// 3. Viết hoàn tất môi trường prod
